613b59168e381ffb5a09fa0312b3d0750fb2e986
Rizky Saputra <rizkysaputra000@gmail.com>
2020-11-08 14:01:49 +0700

    another test

---
+++ client/build/precache-manifest.cf1c82217ea0a921494b9e6d7496b30e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2348bb56d141702811c480bc79b02d2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4e15b47e7cb27ac7ec9",
+    "url": "/static/css/2.9db35e51.chunk.css"
+  },
+  {
+    "revision": "760d2dc3061321f9a470",
+    "url": "/static/css/main.7c16b75a.chunk.css"
+  },
+  {
+    "revision": "a4e15b47e7cb27ac7ec9",
+    "url": "/static/js/2.77c8f705.chunk.js"
+  },
+  {
+    "revision": "6da8cdc98b1ea4a6e1f84621d1585af0",
+    "url": "/static/js/2.77c8f705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "760d2dc3061321f9a470",
+    "url": "/static/js/main.2dc2cf4e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
